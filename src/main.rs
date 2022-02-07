d16990362dabdd6bfa12406125bf1cca98bc45a6
Larson Ashcroft <larsona@outlook.com>
2022-02-06 22:57:52 -0600

    format

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use serenity::model::prelude::{GuildId, Interaction, InteractionResponseType};
 
 use serenity::{
     async_trait,
-    model::{channel::{Message}, gateway::Ready},
+    model::{channel::Message, gateway::Ready},
     prelude::*,
 };
 
@@ -18,7 +18,6 @@ const OWNER_ID: UserId = UserId(134509976956829697); // @typecasto#0517
                                                      // const KICK_CHANNEL: ChannelId = ChannelId(888501834312986635); // yuzu piracy
 const KICK_CHANNEL: ChannelId = ChannelId(889360426998046780); // ok.testing
 const LOG_CHANNEL: ChannelId = ChannelId(923761835583352973);
-
 
 // const KICK_TEXT: &'static str =
 //     "*Now you've done it...*\n\n\
@@ -42,15 +41,22 @@ const LOG_CHANNEL: ChannelId = ChannelId(923761835583352973);
 //     Original message text: \n{}";
 
 async fn generate_kick_private_message(message: &Message, ctx: &Context) -> String {
-    let guild_name = &message.guild(&ctx.cache).await
+    let guild_name = &message
+        .guild(&ctx.cache)
+        .await
         .and_then(|g| Some(g.name))
-        .or( Some(String::from("an unknown guild")) )
+        .or(Some(String::from("an unknown guild")))
         .unwrap(); // Either there is a string here or I've done something terribly wrong
     MessageBuilder::new()
-        .push_line(format!("You've been kicked from {} for being a suspected spambot.", guild_name) )
+        .push_line(format!(
+            "You've been kicked from {} for being a suspected spambot.",
+            guild_name
+        ))
         .push_line("Feel free to rejoin once you've secured your account.")
         // .push_line("You may want to check your recent DMs, to see if your account has sent any sketchy links.")
-        .push_line("Change your password, enable 2FA, and don't click on any sketchy links from now on.")
+        .push_line(
+            "Change your password, enable 2FA, and don't click on any sketchy links from now on.",
+        )
         .build()
 }
 
@@ -69,7 +75,10 @@ fn generate_kick_log_message(message: &Message, could_pm: bool) -> String {
         .push_mono_line(&message.author.tag())
         .push("ID: ")
         .push_mono_line(&message.author.id)
-        .push_line(format!("Sent a PM: {}", if could_pm { "Yes" } else { "No" }))
+        .push_line(format!(
+            "Sent a PM: {}",
+            if could_pm { "Yes" } else { "No" }
+        ))
         .push_line(format!("Avatar: {}", &message.author.face()))
         // .push_line(format!("Date: <t:{}:f>", &Utc::now().timestamp()))
         // .push_line("Original message:")
@@ -78,7 +87,6 @@ fn generate_kick_log_message(message: &Message, could_pm: bool) -> String {
 }
 
 struct Handler;
-
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -101,7 +109,8 @@ impl EventHandler for Handler {
         let private_message_text = generate_kick_private_message(&new_message, &ctx).await;
         if let Ok(dm_channel) = new_message.author.create_dm_channel(&ctx.http).await {
             could_private_message = dm_channel
-                .send_message(&ctx.http, |m| m.content(private_message_text)).await
+                .send_message(&ctx.http, |m| m.content(private_message_text))
+                .await
                 .is_ok();
         } else {
             could_private_message = false;
@@ -110,19 +119,30 @@ impl EventHandler for Handler {
         // Stage 3: Ban
         // Ban them, deleting 1 day of messages and kicking them, then unban them.
         if let Some(guild) = &new_message.guild(&ctx.cache).await {
-            if let Err(_) = guild.ban_with_reason(&ctx.http, &new_message.author.id, 1, "Spambot (autobanned)").await {
+            if let Err(_) = guild
+                .ban_with_reason(&ctx.http, &new_message.author.id, 1, "Spambot (autobanned)")
+                .await
+            {
                 return; // can't ban this user, return.
             }
             let _ = guild.unban(&ctx.http, &new_message.author.id).await;
-        } else {eprintln!("Failed to find guild.")}
+        } else {
+            eprintln!("Failed to find guild.")
+        }
 
         // Stage 4: Log
         // Send a log message, simple.
         if let Some(log_channel) = &ctx.cache.guild_channel(LOG_CHANNEL).await {
-            log_channel.say(&ctx.http, generate_kick_log_message(&new_message, could_private_message)).await
+            log_channel
+                .say(
+                    &ctx.http,
+                    generate_kick_log_message(&new_message, could_private_message),
+                )
+                .await
                 .expect("Failed to send log message.");
+        } else {
+            eprintln!("Failed to find log channel.")
         }
-        else {eprintln!("Failed to find log channel.")}
     }
 
     // Fired when bot is ready
